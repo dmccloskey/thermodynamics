44951c6207aad535b5fa31c9b667a3f4a5fa8d10
dmccloskey <dmccloskey87@gmail.com>
2015-06-20 14:48:08 -0700

    code cleanup round02

--- thermodynamics/thermodynamics.py
+++ thermodynamics/thermodynamics.py
@@ -1,6 +1,8 @@
+# if the comonent-contribution package is not located in lib/site-packages
+# define the location
 import sys
 sys.path.append('C:\\Users\\dmccloskey-sbrg\\Documents\\GitHub\\component-contribution\\component-contribution')
 
-from workspace import ALEWt01
-
-ALEwt01._main_();
+# import the example file and run
+from thermodynamics_examples import aerobicAnaerobic01
+aerobicAnaerobic01._main_();
--- thermodynamics/thermodynamics_data/tsampling/tsampler_run.m
+++
@@ -1,6 +0,0 @@
-% initialize with Tomlab_CPLEX
-load('C:\Users\dmccloskey-sbrg\Documents\MATLAB\tsampling\tsampler.mat')
-initCobraToolbox();
-% sample
-[tsampler_out, mixedFrac] = gpSampler(iDM2014_WTEColi_113C80_U13C20_01_OxicWtGlc, [], [], [], [], [], true);
-[tsampler_out, mixedFrac] = gpSampler(tsampler_out, [], [], [], 20000, [], true);
